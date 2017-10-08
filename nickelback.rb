cd1a5d941f7906922facc01863b7f56e9e0f88dc
Nora Moser <noramoser@Noras-MBP.hsd1.tn.comcast.net>
2017-10-07 20:45:55 -0500

    finished

--- nickelback.rb
+++ nickelback.rb
@@ -3,7 +3,7 @@ small_numbers = []
 
 # Only add numbers to the new list if the value is less than 6
 nums.each {|i| small_numbers.push(i) if i < 6}
-puts small_numbers
+# puts small_numbers
 
 words = ['big', 'red', 'dog', 'ate', 'his', 'food']
 three_letters_words = []
@@ -11,6 +11,24 @@ three_letters_words = []
 # put the 3-letter words in the three_letter_words array using the shovel operator
 for word in words
   if word.length ==3
-    puts three_letters_words << word
+    # puts three_letters_words << word
   end
 end
+
+songs = [
+    ['Nickelback', 'How You Remind Me'],
+    ['Will.i.am', 'That Power'],
+    ['Miles Davis', 'Stella by Starlight'],
+    ['Nickelback', 'Animals']
+]
+better_songs = []
+
+for item in songs 
+    # puts item
+    if item[0] != 'Nickelback'
+        # puts "#{item[1]}"
+        better_songs.push("#{item[1]}")
+    end
+end 
+
+puts better_songs
